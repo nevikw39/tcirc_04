9db9f060342baad60ad5395015ecd466f039624d
nevikw39 <nevikw39@icloud.com>
2020-06-01 16:26:14 +0800

    format

--- 00_satck.cpp
+++ 00_satck.cpp
@@ -7,15 +7,14 @@ int main()
 {
 	stack<int> s;
 
-	for(int i=0; i<10; ++i)
+	for (int i = 0; i < 10; ++i)
 		s.push(i);
-	for(int i=s.size(); i>0; --i)
+	for (int i = s.size(); i > 0; --i)
 	{
 		cout << s.top() << '\n';
 		s.pop();
 	}
-	if(s.empty())
-		cout << "Stack is empty." << '\n';
+	if (s.empty())
+		cout << "Stack is empty.\n";
 	return 0;
 }
-
--- 01_stack_queue.cpp
+++ 01_stack_queue.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <stack>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -8,23 +8,20 @@ int main()
 {
 	stack<int> s;
 	queue<int> q;
-	
-	for(int i=0; i<10; i++)
+
+	for (int i = 0; i < 10; i++)
 	{
 		s.push(i);
 		q.push(i);
 	}
-	for(int i=0; i<10; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		cout << "Stack: " << s.top() << ' '; 
+		cout << "Stack: " << s.top() << ' ';
 		s.pop();
-		cout << "Queue: " << q.front() << ' ' << '\n';
+		cout << "Queue: " << q.front() << '\n';
 		q.pop();
 	}
-	if(q.empty())
-		cout << "Queue is empty." << '\n';
+	if (q.empty())
+		cout << "Queue is empty.\n";
 	return 0;
 }
-
-
-
